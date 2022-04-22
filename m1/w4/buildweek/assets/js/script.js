be8628e65898e3f136ec34863232803071971996
davidullo <davbarbi@gmail.com>
2022-04-22 15:32:55 +0200

    buildweek 11

--- m1/w4/buildweek/assets/js/script.js
+++ m1/w4/buildweek/assets/js/script.js
@@ -1,64 +1,56 @@
 // SLIDER ___________________________________________________________________
 
 
-// let slider = document.querySelector('#slider')
-// let sliderImg = document.querySelector('.slider-img')
-// let width = -(sliderImg.width)
+let slider = document.querySelector('#slider')
+let sliderImg = document.querySelector('.slider-img')
+let width = -(sliderImg.width)
 
-// let fineSlider = width * 7
+let margin = 0
+let currentMargin = 0
 
-// let margin = 0
-// let currentMargin = 0
+if (screen.width < 480) {
+    fineSlider = width * 7
+}
+if (screen.width > 479) {
+    width += 40
+    fineSlider = width * 6
+}
+if (screen.width > 769) {
+    width -= 10
+    fineSlider = width * 5
+}
+if (screen.width > 979) {
+    width -= 30
+    fineSlider = width * 4
+}
 
-
-// if (screen.width < 480) {
-//     fineSlider = width * 7
-// }
-// if (screen.width > 479) {
-//     width += 10
-//     fineSlider = width * 6
-// }
-// if (screen.width > 769) {
-//     width += 20
-//     fineSlider = width * 5
-// }
-// if (screen.width > 979) {
-//     width -= 30
-//     fineSlider = width * 4
-// }
-
-
-// function forward() {
-//     slider.style.marginLeft = currentMargin + width + 'px'
-//     currentMargin = eval((slider.style.marginLeft).replace('px', ''))
-
-//     if (currentMargin < fineSlider) {
-//         console.log('fine');
-//         console.log(-fineSlider);
-//         slider.style.marginLeft = 0 + 'px'
-//         return currentMargin = 0
-//     }
-
-// }
-
-let sliderImgs = document.getElementsByClassName('slider-img')
-let widthImgs = -(sliderImgs[0].width)
+//su chrome funziona su safari no
 
 function forward() {
-    n++
-    if (n > 4) {
-        n = 0
+    slider.style.marginLeft = currentMargin + width + 'px'
+    currentMargin = eval((slider.style.marginLeft).replace('px', ''))
+
+    if (currentMargin < fineSlider) {
+        slider.style.marginLeft = 0 + 'px'
+        currentMargin = 0
     }
 
+    var x = currentMargin / width
+    cambiaColore(x)
 
-    getImg(n)
 }
 
 function getImg(x) {
-    n = x
-    sliderImgs[0].style.transform = "translatex(" + (widthImgs * x) + "px)"
+    slider.style.marginLeft = width * x + 'px'
+    currentMargin = eval((slider.style.marginLeft).replace('px', ''))
+    console.log(currentMargin);
+
+    cambiaColore(x)
+}
+
+function cambiaColore(i) {
     document.querySelector(".dot.active").classList.remove("active")
-    document.getElementsByClassName("dot")[x].classList.add("active")
+    document.getElementsByClassName("dot")[i].classList.add("active")
 }
 
 
