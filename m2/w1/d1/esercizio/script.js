386f2e3eedda6826ffd18e4ea0075b942271578a
davidullo <davbarbi@gmail.com>
2022-04-26 17:36:33 +0200

    esercizio

--- m2/w1/d1/esercizio/script.js
+++ m2/w1/d1/esercizio/script.js
@@ -8,6 +8,21 @@ console.log(dataNascita.getFullYear());
 
 console.log('----- inizio -----')
 
+let listaMesi = [
+    'Gennaio',
+    'Febbraio',
+    'Marzo',
+    'Aprile',
+    'Maggio',
+    'Giugno',
+    'Luglio',
+    'Agosto',
+    'Settembre',
+    'Ottobre',
+    'Novembre',
+    'Dicembre',
+]
+
 console.log(dataAttuale.getDate());
 console.log(dataAttuale.getMonth());
 console.log(dataAttuale.getFullYear());
@@ -25,27 +40,34 @@ console.log(document.querySelector('.secondo .campo').textContent);
 
 console.log('----- esercizio -----');
 
-// document.querySelector('.giorno .campo').textContent = dataAttuale.getDate()
-// document.querySelector('.mese .campo').textContent = dataAttuale.getMonth()
-// document.querySelector('.anno .campo').textContent = dataAttuale.getFullYear()
-// document.querySelector('.ora .campo').textContent = dataAttuale.getHours()
-// document.querySelector('.minuto .campo').textContent = dataAttuale.getMinutes()
-// document.querySelector('.secondo .campo').textContent = dataAttuale.getSeconds()
+/* PRIMO metodo */
 
-setInterval(function () {
-    document.querySelector('.giorno .campo').innerHTML = dataAttuale.getDate();
-    document.querySelector('.mese .campo').innerHTML = dataAttuale.getMonth() + 1;
-    document.querySelector('.anno .campo').innerHTML = dataAttuale.getFullYear();
-    document.querySelector('.ora .campo').innerHTML = dataAttuale.getHours();
-    document.querySelector('.minuto .campo').innerHTML = dataAttuale.getMinutes();
-    document.querySelector('.secondo .campo').innerHTML = dataAttuale.getSeconds();
-}, 1000)
+setInterval(aggiornaData, 1000);
 
-/* Problemi:
-- Il timer dei secondi non si aggiorna ogni secondo
-- Sostituire il numero del mese con il relativo nome
-- Chiarimento "Creare un sistema per la formattazione in formato europeo"
-*/
+function aggiornaData() {
+    const dataCorrente = new Date()
+    document.querySelector('.giorno .campo').innerHTML = dataCorrente.getDate();
+    document.querySelector('.mese .campo').innerHTML = listaMesi[dataCorrente.getMonth()];
+    document.querySelector('.anno .campo').innerHTML = dataCorrente.getFullYear();
+    document.querySelector('.ora .campo').innerHTML = dataCorrente.getHours();
+    document.querySelector('.minuto .campo').innerHTML = dataCorrente.getMinutes();
+    document.querySelector('.secondo .campo').innerHTML = dataCorrente.getSeconds();
+};
+
+/* Fine PRIMO metodo */
+
+/* SECONDO metodo */
+
+// setInterval(function () {
+//     document.querySelector('.giorno .campo').innerHTML = dataAttuale.getDate();
+//     document.querySelector('.mese .campo').innerHTML = listaMesi[dataAttuale.getMonth()];
+//     document.querySelector('.anno .campo').innerHTML = dataAttuale.getFullYear();
+//     document.querySelector('.ora .campo').innerHTML = dataAttuale.getHours();
+//     document.querySelector('.minuto .campo').innerHTML = dataAttuale.getMinutes();
+//     document.querySelector('.secondo .campo').innerHTML = dataAttuale.getSeconds();
+// }, 1000);
+
+/* Fine SECONDO metodo */
 
 const options = {
     weekday: 'long',
