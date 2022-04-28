dddb2424ee8c69b2a3918196f9ad9ea241dea5e7
davidullo <davbarbi@gmail.com>
2022-04-28 17:16:52 +0200

    esercizio

---
+++ m2/w1/d3/esercizio/assets/script/script.js
@@ -0,0 +1,30 @@
+// MENU
+function apriMenu() {
+    document.getElementById('pannello').classList.toggle('apri');
+}
+
+// PARTE AGGIUNGI TO-DO
+let bottone = document.querySelector('#inserisci')
+let toDoArea = document.querySelector('#lista-todo')
+bottone.addEventListener('click', function () {
+    let todoWrap = document.createElement('li')
+    let testo = document.querySelector('#testo')
+    todoWrap.innerText = testo.value
+    toDoArea.append(todoWrap)
+
+    testo.value = ''
+})
+
+// LISTA IMPEGNI
+let lista = document.querySelectorAll('li');
+for (var i = 0; i < lista.length; i++) {
+    lista[i].addEventListener('mouseover', function () {
+        this.classList.add('selezione');
+    });
+    lista[i].addEventListener('mouseout', function () {
+        this.classList.remove('selezione');
+    });
+    lista[i].addEventListener('click', function () {
+        this.classList.toggle('fatto');
+    });
+}
