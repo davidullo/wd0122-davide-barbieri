0a4f84a901e2743368a4ce347445cca8c01bd310
davidullo <davbarbi@gmail.com>
2022-05-26 16:02:30 +0200

    esercizio

---
+++ m3/w1/d4/esercizio/script.js
@@ -0,0 +1,39 @@
+"use strict";
+class Capi {
+    constructor(id, codProd, collezione, capo, modello, quantita, colore, prezzoIvaEsclusa, prezzoIvaInclusa, disponibile, saldo) {
+        this.id = id;
+        this.codProd = codProd;
+        this.collezione = collezione;
+        this.capo = capo;
+        this.modello = modello;
+        this.quantita = quantita;
+        this.colore = colore;
+        this.prezzoIvaEsclusa = prezzoIvaEsclusa;
+        this.prezzoIvaInclusa = prezzoIvaInclusa;
+        this.disponibile = disponibile;
+        this.saldo = saldo;
+    }
+    getSaldoCapo() {
+        let scontoCapo = ((this.prezzoIvaInclusa * this.saldo) / 100);
+        return scontoCapo;
+    }
+    getAcquistoCapo() {
+        let capoScontato = this.prezzoIvaInclusa - this.getSaldoCapo();
+        return capoScontato;
+    }
+}
+const APPURL = 'abbigliamento-new.json';
+fetch(APPURL)
+    .then(res => res.json())
+    .then(res => {
+    for (let capo of res) {
+        capo = new Capi(capo.id, capo.codProd, capo.collezione, capo.capo, capo.modello, capo.quantita, capo.colore, capo.prezzoIvaEsclusa, capo.prezzoIvaInclusa, capo.disponibile, capo.saldo);
+        console.log(`Capo: ${capo.capo}`);
+        console.log(`Disponibilità:  ${capo.disponibile}`);
+        console.log(`Prezzo capo:  ${capo.prezzoIvaInclusa} €`);
+        console.log(`Sconto in percentuale ed euro: ${capo.saldo} % | ${capo.getSaldoCapo()} €`);
+        console.log(`Prezzo del capo scontato: ${capo.getAcquistoCapo()} €`);
+        console.log('---');
+    }
+});
+//# sourceMappingURL=script.js.map
---
+++ m3/w1/d4/esercizio/script.ts
@@ -0,0 +1,55 @@
+class Capi {
+    public id: number
+    public codProd: number
+    public collezione: string
+    public capo: string
+    public modello: number
+    public quantita: number
+    public colore: string
+    public prezzoIvaEsclusa: number
+    public prezzoIvaInclusa: number
+    public disponibile: boolean
+    public saldo: number
+
+    constructor(id: number, codProd: number, collezione: string, capo: string, modello: number, quantita: number, colore: string, prezzoIvaEsclusa: number, prezzoIvaInclusa: number, disponibile: boolean, saldo: number) {
+        this.id = id;
+        this.codProd = codProd;
+        this.collezione = collezione;
+        this.capo = capo;
+        this.modello = modello;
+        this.quantita = quantita;
+        this.colore = colore;
+        this.prezzoIvaEsclusa = prezzoIvaEsclusa;
+        this.prezzoIvaInclusa = prezzoIvaInclusa;
+        this.disponibile = disponibile;
+        this.saldo = saldo;
+    }
+
+    getSaldoCapo(): number {
+        let scontoCapo = ((this.prezzoIvaInclusa * this.saldo) / 100);
+        return scontoCapo;
+    }
+
+    getAcquistoCapo(): number {
+        let capoScontato = this.prezzoIvaInclusa - this.getSaldoCapo();
+        return capoScontato;
+    }
+}
+
+const APPURL: string = 'abbigliamento-new.json'
+
+fetch(APPURL)
+    .then(res => res.json())
+    .then(res => {
+
+        for (let capo of res) {
+            capo = new Capi(capo.id, capo.codProd, capo.collezione, capo.capo, capo.modello, capo.quantita, capo.colore, capo.prezzoIvaEsclusa, capo.prezzoIvaInclusa, capo.disponibile, capo.saldo)
+
+            console.log(`Capo: ${capo.capo}`);
+            console.log(`Disponibilità:  ${capo.disponibile}`);
+            console.log(`Prezzo capo:  ${capo.prezzoIvaInclusa} €`);
+            console.log(`Sconto in percentuale ed euro: ${capo.saldo} % | ${capo.getSaldoCapo()} €`);
+            console.log(`Prezzo del capo scontato: ${capo.getAcquistoCapo()} €`);
+            console.log('---');
+        }
+    })
